398b3438f94a1dac52172c4d87810a6c1c23ed1e
Sean Ransonette <sransonette@gmail.com>
2020-09-09 02:14:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,7 +111,13 @@ class Student
     end.first
   end
   
-  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+       SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade)
+  end
   
   
   
